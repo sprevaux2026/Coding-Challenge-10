51259edccd82029b263fdfe87c81ee2991510d15
sprevaux2026 <stevenprevaux@gmail.com>
2025-02-23 15:26:12 -0500

    Task 2 - Created Order Class

--- cc_10.js
+++ cc_10.js
@@ -24,3 +24,25 @@ console.log(prod1.getDetails());
 
 prod1.updateStock(3);
 console.log(prod1.getDetails());
+
+// Task 2 - Created Order Class
+class Order {
+    constructor(orderId, product, quantity) {
+        this.orderId = orderId;
+        this.product = product;
+        this.quantity = quantity;
+
+        this.product.updateStock(this.quantity);
+    }
+
+    // Get order details
+    getOrderDetails() {
+        return `Order ID: ${this.orderId}, Product: ${this.product.name}, Quantity: ${this.quantity}, Total Price: $${this.product.price * this.quantity}`;
+    }
+}
+
+// Test Case
+const order1 = new Order(601, prod1, 4);
+console.log(order1.getOrderDetails());
+console.log(prod1.getDetails());
+
